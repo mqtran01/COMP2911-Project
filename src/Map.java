3c213673e1003a85adb72d926a8f427cd0eccae2
James Mangos <james.mangos12@gmail.com>
2017-04-30 19:21:30 +1000

    Added a pregenerated map, added in constants for the map state and wrote a testing harness for the map.

--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,16 @@
 public class Map {
-	int[][] grid;
+	int[][] grid;//note: 0,0 is upper left of grid
 	int seed;
 	int player_x;
 	int player_y;
+	
+	final int WALL = 0;
+	final int EMPTY = 1;
+	final int PLAYER = 2;
+	final int BOX = 3;
+	final int GOAL = 4;
+	final int GOALBOX = 5;
+	final int GOALPLAYER = 6;
 	
 	/*grid internal representation
 	 * 0: wall
@@ -21,6 +29,51 @@ public class Map {
 	public Map(int seed){
 		this.seed = seed;
 		grid = newMap(seed);
+	}
+	
+	//pregenerated maps, accessed using characters
+	public Map(char seed){
+		this.grid = new int[6][8];
+		for (int x=0; x < 6; x++){
+			grid[x][0]=WALL;
+			grid[x][7]=WALL;
+		}
+		for (int y=0; y < 8; y++){
+			grid[0][y]=WALL;
+			grid[5][y]=WALL;
+		}
+		grid[1][1] = WALL;
+		grid[2][1] = EMPTY;
+		grid[3][1] = EMPTY;
+		grid[4][1] = WALL;
+		
+		grid[1][2] = PLAYER;
+		grid[2][2] = BOX;
+		grid[3][2] = EMPTY;
+		grid[4][2] = WALL;
+		
+		grid[1][3] = WALL;
+		grid[2][3] = BOX;
+		grid[3][3] = EMPTY;
+		grid[4][3] = WALL;
+		
+		grid[1][4] = WALL;
+		grid[2][4] = EMPTY;
+		grid[3][4] = BOX;
+		grid[4][4] = EMPTY;
+		
+		grid[1][5] = GOAL;
+		grid[2][5] = BOX;
+		grid[3][5] = EMPTY;
+		grid[4][5] = EMPTY;
+		
+		grid[1][6] = GOAL;
+		grid[2][6] = GOAL;
+		grid[3][6] = GOALBOX;
+		grid[4][6] = GOAL;
+		
+		player_x=1;
+		player_y=2;
 	}
 	
 	/**
@@ -50,17 +103,17 @@ public class Map {
 	 * @return
 	 */
 	private int removeObject(int x, int y){
-		if (grid[x][y]==2){
-			return 1;
+		if (grid[x][y]==PLAYER){
+			return EMPTY;
 		}
-		else if (grid[x][y]==3){
-			return 1;
+		else if (grid[x][y]==BOX){
+			return EMPTY;
 		}
-		else if (grid[x][y]==5){
-			return 4;
+		else if (grid[x][y]==GOALBOX){
+			return GOAL;
 		}
-		else if (grid[x][y]==6){
-			return 4;
+		else if (grid[x][y]==GOALPLAYER){
+			return GOAL;
 		}
 		else {
 			System.out.println("This should never occur, trying to remove an object from a grid position with no object");
@@ -69,19 +122,35 @@ public class Map {
 	}
 	
 	public void moveLeft(){
-		moveTo(player_x, player_y, player_x-1, player_y);
+		System.out.println("left");
+		System.out.println(String.format("moveTo(%d,%d,%d,%d)",player_x, player_y, player_x-1, player_y));
+		if (moveTo(player_x, player_y, player_x-1, player_y)){
+			player_x--;
+		}
 	}
 	
 	public void moveRight(){
-		moveTo(player_x, player_y, player_x+1, player_y);
+		System.out.println("right");
+		System.out.println(String.format("moveTo(%d,%d,%d,%d)",player_x, player_y, player_x+1, player_y));
+		if (moveTo(player_x, player_y, player_x+1, player_y)){
+			player_x++;
+		}
 	}
 	
 	public void moveUp(){
-		moveTo(player_x, player_y, player_x, player_y+1);
+		System.out.println("up");
+		System.out.println(String.format("moveTo(%d,%d,%d,%d)",player_x, player_y, player_x, player_y+1));
+		if (moveTo(player_x, player_y, player_x, player_y-1)){
+			player_y--;
+		}
 	}
 	
 	public void moveDown(){
-		moveTo(player_x, player_y, player_x, player_y-1);
+		System.out.println("down");
+		System.out.println(String.format("moveTo(%d,%d,%d,%d)",player_x, player_y, player_x, player_y-1));
+		if (moveTo(player_x, player_y, player_x, player_y+1)){
+			player_y++;
+		}
 	}
 	
 	/**
@@ -95,38 +164,39 @@ public class Map {
 	 * @return true if movement was successful, false otherwise (e.g. if there was a wall in the way)
 	 */
 	private boolean moveTo(int from_x, int from_y, int to_x, int to_y){
-		if (grid[to_x][to_y] == 0){//wall
+		if (grid[to_x][to_y] == WALL){//wall
 			return false;//can never move into a wall
 		}
-		else if (grid[to_x][to_y] == 1){//empty
-			grid[to_x][to_y] = 2;//set the to tile to be a player
+		else if (grid[to_x][to_y] == EMPTY){//empty
+			System.out.println("target is an empty");
+			grid[to_x][to_y] = PLAYER;//set the to tile to be a player
 			grid[from_x][from_y] = removeObject(from_x,from_y);
 			return true;//can always move into an empty space
 		}
-		else if (grid[to_x][to_y] == 2){//player, this should never happen
+		else if (grid[to_x][to_y] == PLAYER){//player, this should never happen
 			System.out.println("error, attempted to move into a space that is occupied by the player");
 			return false;
 		}
-		else if (grid[to_x][to_y] == 3){//box	
+		else if (grid[to_x][to_y] == BOX){//box	
 			//calculate the next grid space over
 			int new_x = 2*to_x-from_x;
 			int new_y = 2*to_y-from_y;
-			if (grid[new_x][new_y] == 1 || grid[new_x][new_y] == 4){//if the next grid space is not blocked
+			if (grid[new_x][new_y] == EMPTY || grid[new_x][new_y] == GOAL){//if the next grid space is not blocked
 				//then move everything
 				grid[from_x][from_y]= removeObject(from_x,from_y);//remove object from from
 				//move the box out and the player in
-				if (grid[to_x][to_y] == 3){//box -> player
-					grid[to_x][to_y] = 2;
+				if (grid[to_x][to_y] == BOX){//box -> player
+					grid[to_x][to_y] = PLAYER;
 				}
 				else{//grid[to_x][to_y] == 5  
-					grid[to_x][to_y]= 6;//box + goal -> player + goal
+					grid[to_x][to_y]= GOALPLAYER;//box + goal -> player + goal
 				}
 				//change third space
-				if (grid[new_x][new_y] == 1){
-					grid[new_x][new_y]= 3;//empty->box
+				if (grid[new_x][new_y] == EMPTY){
+					grid[new_x][new_y]= BOX;//empty->box
 				}
 				else{//grid[to_x][to_y] == 4  
-					grid[new_x][new_y]= 5;//goal -> goal+box
+					grid[new_x][new_y]= GOALBOX;//goal -> goal+box
 				}
 				return true;
 			}
@@ -134,16 +204,28 @@ public class Map {
 				return false;
 			}
 		}
-		else if (grid[to_x][to_y] == 4){//goal space
-			grid[to_x][to_y] = 6;//set the to tile to be a player+goal
+		else if (grid[to_x][to_y] == GOAL){//goal space
+			grid[to_x][to_y] = GOALPLAYER;//set the to tile to be a player+goal
 			grid[from_x][from_y] = removeObject(from_x,from_y);
 			return true;
 		}
-		else if (grid[to_x][to_y] == 5){//goal + box
-			//TODO: change the state
+		else if (grid[to_x][to_y] == GOALBOX){//goal + box
+			int new_x = 2*to_x-from_x;
+			int new_y = 2*to_y-from_y;
+			if (grid[new_x][new_y] == EMPTY){//if the next grid space is not blocked
+				grid[new_x][new_y] = BOX;//move box to next space
+			}
+			else if (grid[new_x][new_y] == GOAL){
+				grid[new_x][new_y] = GOALBOX;//move box to next space
+			}
+			else{
+				return false;//if the next space is blocked then the move fails
+			}
+			grid[to_x][to_y]=GOALPLAYER;//move player to this space
+			grid[from_x][from_y] = removeObject(from_x,from_y);//move player off previous space
 			return true;
 		}
-		else if (grid[to_x][to_y] == 6){//player + goal, this should never happen
+		else if (grid[to_x][to_y] == GOALPLAYER){//player + goal, this should never happen
 			System.out.println("error, attempted to move into a space that is occupied by the player");
 			return false;
 		}
@@ -157,7 +239,7 @@ public class Map {
 	public boolean winState(){
 		for (int x=0; x < grid.length; x++){
 			for (int y=0; y < grid[0].length; y++){
-				if (grid[x][y]==3){//if box not on goal then it is not a win state
+				if (grid[x][y]==BOX){//if box not on goal then it is not a win state
 					return false;
 				}
 			}
--- src/WarehouseBoss.java
+++ src/WarehouseBoss.java
@@ -1,4 +1,44 @@
+import java.util.Scanner;
 
 public class WarehouseBoss {
-
+		public static void main(String[] args){
+			new WarehouseBoss();
+		}
+		
+		WarehouseBoss(){
+			Map map = new Map('a');
+			for (int y=0; y < 8; y++){
+				for (int x=0; x < 6; x++){
+					System.out.print(map.getTile(x, y));
+				}
+				System.out.print("\n");
+			}
+			
+			Scanner sc = new Scanner(System.in);
+			String input;
+			while(!(input = sc.next()).equals("x")){
+				System.out.println(input);
+				if (input.equals("w")){
+					map.moveUp();
+				}
+				else if (input.equals("d")){
+					map.moveRight();
+				}
+				else if (input.equals("s")){
+					map.moveDown();
+				}
+				else if (input.equals("a")){
+					map.moveLeft();
+				}
+				for (int y=0; y < 8; y++){
+					for (int x=0; x < 6; x++){
+						System.out.print(map.getTile(x, y));
+					}
+					System.out.print("\n");
+				}
+				if (map.winState())break;
+			}
+			sc.close();
+			System.out.println("You win");
+		}
 }
